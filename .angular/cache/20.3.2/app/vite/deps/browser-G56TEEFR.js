3bb5170c5332fc6ac2fd9baa3638420007df1c5f
koresolucoes <koresoluciones@outlook.com>
2025-10-16 08:43:15 -0300

    fix

---
+++ .angular/cache/20.3.2/app/vite/deps/browser-G56TEEFR.js
@@ -0,0 +1,17 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch,
+  init_browser
+} from "./chunk-4YQDALOD.js";
+import "./chunk-R327OCYJ.js";
+init_browser();
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
